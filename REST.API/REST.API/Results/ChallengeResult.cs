64209ed272e4dc3be5dbf70067b465696942800e
Oleg Shalygin <oleg.shalygin@amerihome.com>
2016-07-07 14:57:30 -0700

    added test project

--- REST.API/REST.API/Results/ChallengeResult.cs
+++ REST.API/REST.API/Results/ChallengeResult.cs
@@ -21,8 +21,10 @@ namespace REST.API.Results
         {
             Request.GetOwinContext().Authentication.Challenge(LoginProvider);
 
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
-            response.RequestMessage = Request;
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                RequestMessage = Request
+            };
             return Task.FromResult(response);
         }
     }
